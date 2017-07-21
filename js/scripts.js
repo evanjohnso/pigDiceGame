fde84beb5e71f5a461dca021c4d429b0457ecc5c
ej <evanjohnso2@gmail.com>
2017-07-20 23:41:11 -0700

    add backend currentPlayer functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@ function GameBoard () {
   this.gameDie = new Dice ();
   this.players = [];
 }
-
-  //Create dice object with set sides
+//Each player alternates turns automatically
+GameBoard.prototype.currentPlayer = function(rollin) {
+  if (this.players[0].whosTurn === true) {
+    this.players[1].whosTurn === false;
+    this.players[0].calculateTurn(rollin);
+    return ;
+  } else {
+    this.players[0].whosTurn === false;
+    this.players[1].whosTurn === true;
+    this.players[1].calculateTurn(rollin);
+    return ;
+  }
+}
+//Create dice object with set sides
 function Dice () {
   this.sides = 6;
 }
@@ -12,59 +24,51 @@ function Dice () {
 Dice.prototype.diceRoll = function() {
   return Math.floor(Math.random() * this.sides) + 1;
 }
-  //Create number of players
+//Player Object
+function Player (user, turn) {
+  this.user = user;
+  this.whosTurn = turn;
+  this.turn = 0;
+  this.total = 0;
 
-  //NEED TO REFACTOR TO MAKE PLAYER1 AND PLAYER2 MATCH UP TO THE USERNAME OF EACH INDEX OF THE PLAYERS ARRAY
-  // GameBoard.prototype.makePlayer = function(number) {
-  //   for (var i =0; i < number; i++) {
-  //   this.players.push(new Player(i + 1) );
-  //   }
-  // }
+}
+Player.prototype.calculateTurn = function(thisRoll) {
+  //If roll 1 then end
+  if (thisRoll === 1) {
+    // this.whosTurn = false;
+    this.turn = 0;
+    //this alert shows up twice on player1 for some reason, little buggy
+    // alert('you rolled a one homie');
+    return this.whosTurn = false;
+  } else if (this.total >= 100) {
 
-  //Player Object
-  function Player (user) {
-    this.user = user;
-    this.whosTurn =
+    this.turn = 0;
+    this.total = 0;
+
+    } else {
+      this.turn += thisRoll;
+    }
+    return true;
+  }
+
+Player.prototype.hold = function () {
+  this.total += this.turn;
+  if (this.total >= 100) {
+    console.log('congrats nerd you won!');
     this.turn = 0;
     this.total = 0;
   }
-
-  Player.prototype.calculateTurn = function(thisRoll) {
-    //If roll 1 then end
-    if (thisRoll === 1) {
-      alert('Your turn is over!');
-      this.whosTurn = 0;
-      this.turn = 0;
-      return false;
-    } else {
-      this.turn += thisRoll;
-      if (this.total >= 100) {
-        this.turn = 0;
-        this.total = 0;
-        alert('You win!!!!!!');
-        return true;
-      }
-    }
-  }
-  Player.prototype.hold = function () {
-    this.total += this.turn;
-    if (this.total >= 100) {
-      this.turn = 0;
-      this.total = 0;
-      alert('You win!');
-    }
-    return this.turn = 0;
-  }
+  return this.turn = 0;
+}
 
 //User Interface
 $(document).ready(function() {
-  //When document loads, create gameDie
   var game =  new GameBoard ();
   var players = [];
   // Choose one player or two
   $('#onePlayer').click(function() {
-    var player1 = new Player ('Player1');
-    var cpu = new Player ('HAL');
+    var player1 = new Player ('Player1', true);
+    var cpu = new Player ('HAL', false);
     //empty array so index's later match up
     players.length = 0;
     players.push(player1, cpu);
@@ -72,29 +76,28 @@ $(document).ready(function() {
     game.players.push(player1, cpu);
     $('.col-md-6').show();
     $('.player2Column h3').text('Hello, my name is HAL.');
-    $('.square2 + p').text("HAL's Total:");
-    console.log(players);
+    // $('.square2 + p').text("Hal's turn: ");
+
     $('.btn-danger').hide();
   });
+
   $('#twoPlayers').click(function() {
-    var player1 = new Player ('Player1');
-    var player2 = new Player ('Player2');
-    //empty array so index's match up
+    //Create two players
+    var player1 = new Player ('Player1', true);
+    var player2 = new Player ('Player2', false);
+    //Empty array so index's match up
     players.length = 0;
     players.push(player1, player2);
     game.players.length = 0;
     game.players.push(player1, player2);
-    console.log(players);
     $('.col-md-6').show();
     $('.btn-danger').hide();
   });
-
   //Player one roll random number and add together current turn unless user rolls a 1
   $('.player1').click(function() {
-    console.log(players);
-    console.log(game.players);
     //Access dice roll and roll it
     var thisRoll = game.gameDie.diceRoll();
+    game.currentPlayer(thisRoll);
     var turn = game.players[0].calculateTurn(thisRoll);
     if (turn === false) {
       $('.btn-info').show();
@@ -105,6 +108,7 @@ $(document).ready(function() {
     $('.square1').text(thisRoll);
     $('.user1Turn').text(game.players[0].turn);
     $('.user1Total').text(game.players[0].total);
+    (game.players);
   });
   //Player 1 save turn and add to total, reset fields
   $('.player1Hold').click(function () {
